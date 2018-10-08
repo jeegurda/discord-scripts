fdbb646c6eb94896c0b486ea48f0b34e10ad5a4a
Shagen A <asriyansh@gmail.com>
2018-10-08 06:25:24 +0300

    initial

---
+++ delete-stuff.js
@@ -0,0 +1,49 @@
+;(async () => {
+  // Change me
+  var channel = '189322461374840832'
+  var authorization = 'token'
+  var username = 'carl'
+  var history = 1000
+  var dry = false
+
+
+  var limit = 100
+  var messages = []
+
+  while (history > 0) {
+    history -= limit
+
+    var before = messages[messages.length - 1]
+
+    await fetch(`/api/v6/channels/${channel}/messages?limit=${limit}${before ? `&before=${before.id}` : ''}`, {
+      headers: new Headers({ authorization })
+    })
+      .then(s => s.json())
+      .then(d => d.length
+        ? messages = messages.concat(d)
+        : history = 0
+      )
+
+    console.log(`Received ${messages.length} messages`)
+  }
+
+  var userMessages = messages
+    .filter(i => i.author.username === username)
+    .map(m => m.id)
+
+  console.log(`Found ${userMessages.length} your messages`)
+
+  if (dry) {
+    return
+  }
+
+  for (var i of userMessages) {
+    await fetch(`/api/v6/channels/${channel}/messages/${i}`, {
+      headers: new Headers({ authorization }),
+      method: 'DELETE'
+    })
+    await new Promise(res => setTimeout(res, 300))
+  }
+
+  console.log('Done!')
+})()
