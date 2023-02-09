ece77ea054ccdc391772e09284226869670c91f6
Shagen A <asriyan.sh@gmail.com>
2023-02-09 22:44:36 +0300

    rate limit

--- delete-stuff.js
+++ delete-stuff.js
@@ -1,47 +1,50 @@
 ;(async () => {
-  var channel = '189322461374840832' // Your channel
-  var authorization = 'token' // Your auth token
-  var username = 'carl' // Username to match
-  var history = 1000 // How many messages to fetch (total)
-  var dry = true // Change to false to actually send DELETE requests
+  const channel = '268087097641009162' // Your channel
+  const authorization = '' // Your auth token
+  const username = 'jeegurda' // Username to match
+  let history = 1000 // How many messages to fetch (total)
+  const dry = true // Change to false to actually send DELETE requests
 
-
-  var limit = 100
-  var messages = []
+  const limit = 100
+  let messages = []
 
   while (history > 0) {
     history -= limit
 
-    var before = messages[messages.length - 1]
+    const before = messages[messages.length - 1]
 
-    await fetch(`/api/v6/channels/${channel}/messages?limit=${limit}${before ? `&before=${before.id}` : ''}`, {
-      headers: new Headers({ authorization })
-    })
-      .then(s => s.json())
-      .then(d => d.length
-        ? messages = messages.concat(d)
-        : history = 0
-      )
+    await fetch(
+      `/api/v6/channels/${channel}/messages?limit=${limit}${
+        before ? `&before=${before.id}` : ''
+      }`,
+      {
+        headers: new Headers({ authorization }),
+      },
+    )
+      .then((s) => s.json())
+      .then((d) => (d.length ? (messages = messages.concat(d)) : (history = 0)))
 
     console.log(`Received ${messages.length} messages`)
   }
 
-  var userMessages = messages
-    .filter(i => i.author.username === username)
-    .map(m => m.id)
+  const userMessages = messages
+    .filter((i) => i.author.username === username)
+    .map((m) => m.id)
 
-  console.log(`Found ${userMessages.length} your messages`)
+  console.log(
+    `Found ${userMessages.length} your messages. Out of %o. Check username if 0`,
+  )
 
   if (dry) {
     return
   }
 
-  for (var i of userMessages) {
+  for (let i of userMessages) {
     await fetch(`/api/v6/channels/${channel}/messages/${i}`, {
       headers: new Headers({ authorization }),
-      method: 'DELETE'
+      method: 'DELETE',
     })
-    await new Promise(res => setTimeout(res, 300))
+    await new Promise((res) => setTimeout(res, 300))
   }
 
   console.log('Done!')
